438fb8b9a25402cbbfa1ac002b61cb18adeccbb1
a-wing <1@233.email>
2018-10-17 20:25:40 +0800

    Add bin/lilackiss unix.sock

--- config-dist.sh
+++ config-dist.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
 
+### lilac
+LILAC_LOG=build.log
+LILAC_KISS_LOG=log/lilackiss.log
+
+### kiss2u base
 export KISS2U_AUTH_KEY=
-export RAILS_ENV=
+export RAILS_ENV=production
 export SECRET_KEY_BASE=
-PORT=
+
+### kiss2u localhost:port || unin.sock
+PORT=3000
+#SOCK=
