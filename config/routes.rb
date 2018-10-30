47cabafc342edacf3c448985bf24ecf995fd5d25
a-wing <1@233.email>
2018-10-30 21:55:16 +0800

    Fix /packages/ . bug #8

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-  resources :packages, only: [:index, :show, :create]
+  resources :packages, only: [:index, :create]
+  get 'packages/:id', to: 'packages#show', constraints: { id: /[\w, \-, \.]*/ }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
