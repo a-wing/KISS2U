843cf880532753ff7e07723e22fe311f9be04ffa
a-wing <1@233.email>
2018-11-05 21:18:35 +0800

    Add Test packages

--- test/controllers/packages_controller_test.rb
+++ test/controllers/packages_controller_test.rb
@@ -1,7 +1,14 @@
 require 'test_helper'
 
 class PackagesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "get index" do
+    get packages_path
+    assert_response :success
+  end
+
+  test "get show" do
+    get '/api/packages/blender-2.8-git'
+    #puts JSON.parse(response.body)
+    assert_response :success
+  end
 end
