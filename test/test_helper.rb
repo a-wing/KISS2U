17187ca1202081ca5533812910db300a7a27294a
a-wing <1@233.email>
2018-11-06 09:45:27 +0800

    Add codecov

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,6 +2,12 @@ ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 require 'rails/test_help'
 
+require 'simplecov'
+SimpleCov.start
+
+require 'codecov'
+SimpleCov.formatter = SimpleCov::Formatter::Codecov
+
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
